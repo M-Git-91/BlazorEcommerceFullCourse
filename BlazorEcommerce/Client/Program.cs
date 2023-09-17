d9f7ac61a5076e35e1d2e2141561959594673003
Mirec91 <adkgombasky@centrum.sk>
2023-09-17 11:28:29 +0200

    Product categories client side

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -1,5 +1,7 @@
 global using BlazorEcommerce.Client;
 global using BlazorEcommerce.Client.Services.ProductService;
+global using BlazorEcommerce.Shared;
+using BlazorEcommerce.Client.Services.CategoryService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 await builder.Build().RunAsync();
