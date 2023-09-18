88b30fac2742b167d727f2ed4dc22678fe6c78be
Mirec91 <adkgombasky@centrum.sk>
2023-09-18 18:55:41 +0200

    Searchbar and navbar

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -13,6 +13,7 @@ namespace BlazorEcommerce.Client.Services.ProductService
         }
 
         public List<Product> Products { get; set; } = new List<Product>();
+        public string Message { get; set; } = "Loading products...";
 
         public event Action ProductChanged;
 
@@ -33,6 +34,26 @@ namespace BlazorEcommerce.Client.Services.ProductService
             }
 
             ProductChanged.Invoke();
+        }
+
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}"); 
+            if(result != null && result.Data != null ) 
+            {
+                Products = result.Data;
+            }
+            if (Products.Count == 0) 
+            {
+                Message = "No products found.";
+            }
+            ProductChanged?.Invoke();
         }
     }
 }
