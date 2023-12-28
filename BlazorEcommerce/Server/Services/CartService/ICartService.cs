6987730e044cd478ed3edb71d14165c43e26172b
Mirec91 <adkgombasky@centrum.sk>
2023-12-28 13:48:06 +0100

    152. Fulfill order and store order in DB.

--- BlazorEcommerce/Server/Services/CartService/ICartService.cs
+++ BlazorEcommerce/Server/Services/CartService/ICartService.cs
@@ -7,7 +7,7 @@ namespace BlazorEcommerce.Server.Services.CartService
         Task<ServiceResponse<List<CartProductResponse>>> GetCartProducts(List<CartItem> cartItems);
         Task<ServiceResponse<List<CartProductResponse>>> StoreCartItems(List<CartItem> cartItems);
         Task<ServiceResponse<int>> GetCarItemsCount();
-        Task<ServiceResponse<List<CartProductResponse>>> GetDbCartProducts();
+        Task<ServiceResponse<List<CartProductResponse>>> GetDbCartProducts(int? userId = null);
         Task<ServiceResponse<bool>> AddToCart(CartItem cartItem);
         Task<ServiceResponse<bool>> UpdateQuantity(CartItem cartItem);
         Task<ServiceResponse<bool>> RemoveItemFromCart(int productId, int productTypeId);
--- BlazorEcommerce/Server/Services/PaymentService/IPaymentService.cs
+++ BlazorEcommerce/Server/Services/PaymentService/IPaymentService.cs
@@ -5,6 +5,7 @@ namespace BlazorEcommerce.Server.Services.PaymentService
     public interface IPaymentService
     {
         Task<Session> CreateCheckoutSession();
+        Task<ServiceResponse<bool>> FulfillOrder(HttpRequest request);
 
     }
 }
