da9f8e7362a3a95a757a69ffb673608dc96c58fc
Mirec91 <adkgombasky@centrum.sk>
2024-01-02 16:14:04 +0100

    194. Update and remove product images.

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -195,7 +195,10 @@ namespace BlazorEcommerce.Server.Services.ProductService
 
         public async Task<ServiceResponse<Product>> UpdateProduct(Product product)
         {
-            var dbProduct = await _context.Products.FindAsync(product.Id);
+            var dbProduct = await _context.Products
+                .Include(p => p.Images)
+                .FirstOrDefaultAsync(p => p.Id == product.Id);
+
             if (dbProduct == null)
             {
                 return new ServiceResponse<Product>
@@ -211,6 +214,10 @@ namespace BlazorEcommerce.Server.Services.ProductService
             dbProduct.CategoryId = product.CategoryId;
             dbProduct.Visible = product.Visible;
             dbProduct.Featured = product.Featured;
+
+            var productImages = dbProduct.Images;
+            _context.Images.RemoveRange(productImages);
+            dbProduct.Images = product.Images;
 
             foreach (var variant in product.Variants)
             {
